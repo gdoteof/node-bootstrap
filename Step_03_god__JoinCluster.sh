cee8a1d8b07172b183a89e2783944a1bf01635e3
gdoteof <gdoteof@gmail.com>
2023-05-31 19:08:11 -0400

    right

--- Step_03_god__JoinCluster.sh
+++ Step_03_god__JoinCluster.sh
@@ -15,6 +15,7 @@ expect_geoff_creds
 
 
 copyk3sConfig
-copyHelmConfig
+copyHelmAddons
+
 
 curl -sfL https://get.k3s.io | K3S_TOKEN=$GEOFF_K3S_TOKEN K3S_SERVER=$GEOFF_K3S_SERVER sh -s - server
--- Step_03_init__JoinCluster.sh
+++ Step_03_init__JoinCluster.sh
@@ -13,7 +13,7 @@ TOKEN=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 64 ; echo '')
 echo using $TOKEN
 
 copyk3sConfig
-copyHelmConfig
+copyHelmAddons
 
 curl -sfL https://get.k3s.io | K3S_TOKEN=$TOKEN sh -s - server --cluster-init
 
