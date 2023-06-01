abeba533e09228d84b2c7d808124a1bc94b57804
gdoteof <gdoteof@gmail.com>
2023-05-31 20:40:18 -0400

    better

--- Step_03_worker__JoinCluster.sh
+++ Step_03_worker__JoinCluster.sh
@@ -5,7 +5,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 . "$SCRIPT_DIR/__common_functions.sh"
 
 check_root
-/usr/local/bin/k3s-uninstall.sh || echo "no previous k3s found"
+/usr/local/bin/k3s-uninstall.sh || echo "no previous k3s server found"
+/usr/local/bin/k3s-killall.sh || echo "no previous k3s agent stuff found"
+/usr/local/bin/k3s-agent-uninstall.sh || echo "no previous k3s agent stuff found"
 
 parse_creds "$@"
 expect_geoff_creds
@@ -15,4 +17,5 @@ copyk3sConfig
 
 
 
+echo "Join with token $GEOFF_K3S_TOKEN to server $GEOFF_K3S_SERVER"
 curl -sfL https://get.k3s.io | K3S_TOKEN=$GEOFF_K3S_TOKEN K3S_SERVER=$GEOFF_K3S_SERVER sh -s - agent
