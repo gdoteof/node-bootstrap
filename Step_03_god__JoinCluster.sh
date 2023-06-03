f77cabb3842bf1a392afc7c9d28e76bed69d23bf
gdoteof <gdoteof@gmail.com>
2023-06-03 03:37:20 -0400

    improve

--- Step_03_god__JoinCluster.sh
+++ Step_03_god__JoinCluster.sh
@@ -6,6 +6,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 check_root
 
+IP=$(ifconfig | grep "inet 10.10.1" | awk '{print $2}' | cut -d ':' -f2 | head -n 1)
+if [ -z "$IP" ]; then
+  echo "Error: No IP address found for 10.10.1.x network"
+  exit 1
+fi
+
 /usr/local/bin/k3s-uninstall.sh || echo "no previous k3s found"
 
 parse_creds "$@"
@@ -20,3 +26,9 @@ copyHelmAddons
 
 curl -sfL https://get.k3s.io | K3S_TOKEN=$GEOFF_K3S_TOKEN sh -s - server --server https://$GEOFF_K3S_SERVER:6443
 
+
+echo "./Step_03_god__JoinCluster.sh --geoff-k3s-token $TOKEN --geoff-k3s-server $GEOFF_K3S_SERVER"
+echo "./Step_03_god__JoinCluster.sh --geoff-k3s-token $TOKEN --geoff-k3s-server $IP"
+
+echo "./Step_03_worker__JoinCluster.sh --geoff-k3s-token $TOKEN --geoff-k3s-server $GEOFF_K3S_SERVER"
+echo "./Step_03_worker__JoinCluster.sh --geoff-k3s-token $TOKEN --geoff-k3s-server $IP"
--- Step_03_init__JoinCluster.sh
+++ Step_03_init__JoinCluster.sh
@@ -7,6 +7,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 check_root
 
+IP=$(ifconfig | grep "inet 10.10.1" | awk '{print $2}' | cut -d ':' -f2 | head -n 1)
+if [ -z "$IP" ]; then
+  echo "Error: No IP address found for 10.10.1.x network"
+  exit 1
+fi
+
 /usr/local/bin/k3s-uninstall.sh || echo "no previous k3s found"
 
 TOKEN=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 64 ; echo '')
@@ -17,9 +23,8 @@ copyHelmAddons
 
 curl -sfL https://get.k3s.io | K3S_TOKEN=$TOKEN sh -s - server --cluster-init
 
-
 echo "if that worked you should be able to do:"
-GEOFF_K3S_SERVER=10.10.1.2
+GEOFF_K3S_SERVER=$IP
 echo "./Step_03_god__JoinCluster.sh --geoff-k3s-token $TOKEN --geoff-k3s-server $GEOFF_K3S_SERVER"
 echo "or"
 echo "./Step_03_worker__JoinCluster.sh --geoff-k3s-token $TOKEN --geoff-k3s-server $GEOFF_K3S_SERVER"
