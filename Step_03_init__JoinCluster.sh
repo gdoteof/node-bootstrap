bef9e116bd93f5e6ec436271e8194b9786f24b62
gdoteof <gdoteof@gmail.com>
2023-06-03 04:02:56 -0400

    maybe

--- Step_03_init__JoinCluster.sh
+++ Step_03_init__JoinCluster.sh
@@ -15,12 +15,9 @@ fi
 
 /usr/local/bin/k3s-uninstall.sh || echo "no previous k3s found"
 
-TOKEN=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 64 ; echo '')
-echo using $TOKEN
-
 copyk3sConfigGod
 
-curl -sfL https://get.k3s.io | K3S_TOKEN=$TOKEN sh -s - server --cluster-init
+curl -sfL https://get.k3s.io |sh -s - server --cluster-init
 
 echo "if that worked you should be able to do:"
 GEOFF_K3S_SERVER=$IP
